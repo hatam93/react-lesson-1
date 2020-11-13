eebe891a244b3ef58b3a0bb0ea55e5ecfe2c1431
hatam <rolexnatiomzm@gmail.com>
2020-11-13 16:14:31 +0600

    add react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/header/header';
+import Hero from './components/header/hero';
+import './base.scss';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function hero() {
+   return (
+      <section className="hero">
+         <div className="container">
+            <div className="hero__content">
+               <div className="hero__content">
+                  
+               </div>
+            </div>
+         </div>
+      </section>
+   )
+}
+
+export default hero
